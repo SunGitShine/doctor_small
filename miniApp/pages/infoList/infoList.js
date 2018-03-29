4c7677272925ea0263cca5752c8cb77592087a85
Qiu Kevin <329926598@qq.com>
2018-03-29 23:19:04 +0800

    new project

---
+++ miniApp/pages/infoList/infoList.js
@@ -0,0 +1,99 @@
+const app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    navbar:["医生专栏","医院专栏"],
+    currentTab:0,
+    msg:"this id tabBar",
+    listItems:[
+      {
+        name:"胡萝卜"
+      },{
+        name: "青菜"
+      },{
+        name:"茄子"
+      }
+    ],
+    listDoctorItems:[
+      {
+        name:"西瓜"
+      },
+      {
+        name: "菠萝"
+      }
+    ]
+  },
+  // 响应点击导航栏
+  navbarTap:function(e){
+    var _this = this;
+    _this.setData({
+      currentTab:e.currentTarget.dataset.idx,
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log(options.id);
+    // wx.request({
+    //   url: app.global.commonBaseUrl+"",
+    //   method:"GET",
+    //   dataType:"json",
+    //   data:{},
+    //   success:function(res){
+    //   }
+    // })    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  },
+})
